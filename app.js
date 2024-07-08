2543aec6e06ea6bd27b292c06fbe82625908a880
Jesús Cisneros <chucho.cisneros@hotmail.com>
2024-07-07 21:43:31 -0600

    array

--- app.js
+++ app.js
@@ -1,64 +1,76 @@
-let numeroSecreto = generararNumeroSecreto();
-let numeroIntentos = 1;
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
-console.log(numeroSecreto);
-let parrafo = document.querySelector("p");
-parrafo.innerHTML = "Indica un número del 1 al 10";
+
 
 function asignarTextoElemento(elemento, texto) {
-  let titulo = document.querySelector(elemento);
-  titulo.innerHTML = texto;
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
 
-function verificarintento() {
-  let numeroUsuario = parseInt(document.getElementById("valorUsuario").value);
-  console.log(numeroIntentos);
-
-  if (numeroUsuario == numeroSecreto) {
-    asignarTextoElemento(
-      "h1",
-      `¡Felicidades! ¡Adivinaste el número secreto! en ${numeroIntentos} ${
-        numeroIntentos === 1 ? "vez" : "veces"
-      }`
-    );
-    document.getElementById("reiniciar").removeAttribute("disabled");
-  } else {
-    if (numeroUsuario > numeroSecreto) {
-      asignarTextoElemento("h1", "El número secreto es menor");
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-      asignarTextoElemento("h1", "El número secreto es mayor");
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
     }
-    numeroIntentos++;
-    limpiarCaja();
-  }
+    return;
 }
 
 function limpiarCaja() {
-  document.querySelector("#valorUsuario").value = "";
+    document.querySelector('#valorUsuario').value = '';
 }
 
-function generararNumeroSecreto() {
-  return Math.floor(Math.random() * 10 + 1);
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
 }
 
 function condicionesIniciales() {
-  asignarTextoElemento("h1", "Juego del número secreto!");
-  asignarTextoElemento("p", "Indica un número del 1 al 100");
-  numeroSecreto = generararNumeroSecreto();
-  intentos = 1;
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
 }
 
-function reiniciarjuego() {
-  //limpiar caja
-  limpiarCaja();
-
-  //indicar mensaje de intervalos de nímeros
-  condicionesIniciales();
-  //Generar el numero aleatori
-  numeroSecreto = generararNumeroSecreto();
-  //indicar numero de intentos
-  intentos = 1;
-  //Deshabilitar el botón de nuevo juego
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
 }
 
 condicionesIniciales();
